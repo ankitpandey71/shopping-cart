0f0bc1e6516177b3631c0f2e8b5102face29a26a
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2024-07-18 17:59:08 +0530

    [ADD] navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,11 @@
-import { Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-import About from "../pages/About";
 
 const Navbar = () => {
   return (
     <NavbarBs className="bg-white shadow-sm mb-3">
       <Container>
-        <Nav>
+        <Nav className="me-auto">
           <Nav.Link to="/" as={NavLink}>
             Home
           </Nav.Link>
@@ -14,9 +13,56 @@ const Navbar = () => {
             Store
           </Nav.Link>
           <Nav.Link to="/about" as={NavLink}>
-            Home
+            About
           </Nav.Link>
         </Nav>
+        <Button
+          style={{ width: "3rem", height: "3rem", position: "relative" }}
+          variant="outline-secondary"
+        >
+          <svg
+            width="45px"
+            height="45px"
+            viewBox="4 3 30 30"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2 3L2.26491 3.0883C3.58495 3.52832 4.24497 3.74832 4.62248 4.2721C5 4.79587 5 5.49159 5 6.88304V9.5C5 12.3284 5 13.7426 5.87868 14.6213C6.75736 15.5 8.17157 15.5 11 15.5H13M19 15.5H17"
+              stroke="#1C274C"
+              stroke-width="1.5"
+              stroke-linecap="round"
+            />
+            <path
+              d="M7.5 18C8.32843 18 9 18.6716 9 19.5C9 20.3284 8.32843 21 7.5 21C6.67157 21 6 20.3284 6 19.5C6 18.6716 6.67157 18 7.5 18Z"
+              stroke="#1C274C"
+              stroke-width="1.5"
+            />
+            <path
+              d="M16.5 18.0001C17.3284 18.0001 18 18.6716 18 19.5001C18 20.3285 17.3284 21.0001 16.5 21.0001C15.6716 21.0001 15 20.3285 15 19.5001C15 18.6716 15.6716 18.0001 16.5 18.0001Z"
+              stroke="#1C274C"
+              stroke-width="1.5"
+            />
+            <path
+              d="M5 6H8M5.5 13H16.0218C16.9812 13 17.4609 13 17.8366 12.7523C18.2123 12.5045 18.4013 12.0636 18.7792 11.1818L19.2078 10.1818C20.0173 8.29294 20.4221 7.34853 19.9775 6.67426C19.5328 6 18.5054 6 16.4504 6H12"
+              stroke="#1C274C"
+              stroke-width="1.5"
+              stroke-linecap="round"
+            />
+          </svg>
+          <div
+            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+            style={{
+              color: "white",
+              width: "1.5rem",
+              height: "1.5rem",
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              transform: "translate(25%,25%)",
+            }}
+          ></div>
+        </Button>
       </Container>
     </NavbarBs>
   );
